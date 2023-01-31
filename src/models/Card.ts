86b19a5ee2a3b094d69684d256c6cd1d248b822c
hugoloennqvist <hugolonnqvistosterberg@gmail.com>
2023-01-31 20:46:03 +0100

    Fix to class Card

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -2,10 +2,12 @@ export default class Card {
   label: string;
   value: number;
   suit: string;
+  suitSymbol: string;
 
-  constructor(label: string, value: number, suit: string) {
+  constructor(label: string, value: number, suit: string, suitSymbol: string) {
     this.label = label;
     this.value = value;
     this.suit = suit;
+    this.suitSymbol = suitSymbol;
   }
 }
